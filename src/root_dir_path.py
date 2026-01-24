38b765e44a1763ba212655d3610db51ab8e5c8ed
alpassss <107598603+alpassss@users.noreply.github.com>
2026-01-25 00:36:19 +0800

    Update root_dir_path.py

--- src/root_dir_path.py
+++ src/root_dir_path.py
@@ -2,4 +2,4 @@ import os
 
 # ROOT_DIR should point to the PRAG repository root directory
 # Update this path according to your environment
-ROOT_DIR = os.environ.get("PRAG_ROOT", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = '/kaggle/working/PRAG'
